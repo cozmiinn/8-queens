59c27c3198adc038fcff87bf99706c77b393b5bf
Mihalescul Cosmin <cosmin.mihalescul@student.upt.ro>
2022-11-30 12:27:43 +0200

    add generate board function

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ solveNQ();
 
 function direction(board, row, col) {
     //checks left direction
-    for(var i = 0; i < col; i++) {
+    for (var i = 0; i < col; i++) {
         if (board[row][i] == 1) {
             return false;
         }
@@ -24,18 +24,34 @@ function direction(board, row, col) {
     return true;
 }
 
-function recursive(board, col){
-    if(col === n){
+function recursive(board, col) {
+    if (col === n) {
         printfSolution(board);
         //print another solution when the n=8
         return;
     }
-    for(var i = 0; i < n; i++){
-        if(direction(board, i, col)){
+    for (var i = 0; i < n; i++) {
+        if (direction(board, i, col)) {
             board[i][col] = 1;
             recursive(board, col + 1);
             board[i][col] = 0;
         }
     }
     return false;
+}
+
+function solve() {
+    var board = genBoard(n);
+    recursive(board, 0);
+}
+
+function genBoard(n) {
+    var board = [];
+    for (var i = 0; i < n; i++) {
+        board[i] = [];
+        for (var j = 0; j < n; j++) {
+            board[i][j] = 0;
+        }
+    }
+    return board;
 }
